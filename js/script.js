0f87c6d6f58bcea8ab3551bb8772cc38c90cc367
Agustin Melfi <agu.melfi@gmail.com>
2025-04-07 17:30:52 -0300

    Archivos principales.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Variable: Espacio de memoria reservado para almacenar información importante del programa 
+
+//Declaracion: 
+let cliente;
+
+//Asignacion de valores:
+cliente = "Carlos";
+
+cliente = "Samuel";
+console.log(cliente);
+
+//Inicializar variables:
+let edad = 37;
+
+console.log(edad);
+
+//Const: Una variable cuyo valor no puede ser modificado una vez que ha sido asignado
+
+const nacimiento = 1987;
+
+console.log(nacimiento);
+
+//
+
+let inscripto = true;
+
+console.log(inscripto);
